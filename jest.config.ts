953976164ded2450a43ba0251cd940a19e0be475
MaSTerAK-902 <74452307+MaSTerAK-902@users.noreply.github.com>
2024-07-04 02:38:18 +0000

    chore: Add Jest setup and configuration files, update package.json dependencies

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jest-environment-jsdom',
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+export default createJestConfig(config);
