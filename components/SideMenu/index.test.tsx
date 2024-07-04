d8a792fe82700a64446e72f425cda68886bc7897
MaSTerAK-902 <74452307+MaSTerAK-902@users.noreply.github.com>
2024-07-04 02:53:31 +0000

    chore: Add SideMenu component and update layout to include it

---
+++ components/SideMenu/index.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+
+describe('サイドバーテスト', () => {
+    test('SideMenu ', () => {
+    //   const { getByRole } = render(<HomePresentation message="Hello Jest!!" />);
+    //   expect(getByRole('heading', { name: 'Hello Jest!!' })).toBeInTheDocument();
+    });
+  });
---
+++ components/SideMenu/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css';
+
+export default function SideMenu() {
+    return (
+        <>
+            <p className={styles.test}>Hello World.</p>
+        </>
+    );
+};
