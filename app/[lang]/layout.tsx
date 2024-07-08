8c0734cb7c6b66f093c62a3c91eec5bdf7321789
MaSTerAK-902 <74452307+MaSTerAK-902@users.noreply.github.com>
2024-07-08 07:09:55 +0000

    chore: Update page styles and layout to include a new CSS class for the content title and increase its font size to 2xl

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from "next";
-import { i18n, type Locale } from "../../i18n-config";
 import "./globals.css";
+import type { Metadata } from "next";
+import { i18n, type Locale } from "@/i18n-config";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,15 +1,15 @@
 import { getDictionary } from "@/get-dictionary";
 import { Locale } from "@/i18n-config";
 
-export default async function IndexPage({ params: { lang } }: { params: { lang: Locale }; }){
-    const dictionary = await getDictionary(lang);
+export default async function IndexPage({ params: { lang } }:{ params: { lang: Locale }; }){
+    // Any型を要修正
+    const dictionary: { [key: string]: any } = await getDictionary(lang);
     return (
-        <div>
-            <p className="mt-40">Current locale: {lang}</p>
-            <p>
-            This text is rendered on the server:{" "}
-            {dictionary["server-component"].welcome}
-            </p>
-        </div>
+        <section className="w-[1024px] mx-auto mt-32 px-8 mb-8">
+            <div>
+                <p>Current locale: {lang}</p>
+                <p>This text is rendered on the server:{dictionary["server-component"].welcome}</p>
+            </div>
+        </section>
     );
 }
